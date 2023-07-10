391b3181a2d25826ef2231787c821de5916a228b
monkeyking <2018121152@yonsei.ac.kr>
2023-07-10 19:40:02 +0900

    template mod

--- templates/skeleton.java
+++ templates/skeleton.java
@@ -2,15 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+	static int n, m;
 	public static void main (String[] args) throws IOException {
-		String buf = "";
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int n, sol = 0;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		sb.append("");
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
 		//input
-		//n = Integer.parseInt(st.nextToken());
-		System.out.println(n);
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		bw.write(sb.toString());
+		bw.flush();
 		return ;
 	}
 }
+
